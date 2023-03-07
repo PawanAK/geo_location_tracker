0bf0348d0fad7390f80cb1659e65f34a545e4a24
Pawan Ajjar K <pawanajjark@gmail.com>
2023-03-07 18:31:26 +0530

    this is all the changes iamde

---
+++ script.js
@@ -0,0 +1,8 @@
+// https://geo.ipify.org/
+// const api_key = at_tJWirezro9DWBuDi45jHYzsqA7qTw;;
+//https://geo.ipify.org/api/v2/country,city?apiKey=at_tJWirezro9DWBuDi45jHYzsqA7qTw&ipAddress=8.8.8.8
+
+// https://leafletjs.com/
+
+
+fetch("https://api.ipify.org/").then((r) => r.json()).then(console.log);
