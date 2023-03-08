5357a76ff3d9afd8a2898196bb66abe4b6f2a0c9
Pawan Ajjar K <pawanajjark@gmail.com>
2023-03-08 07:23:14 +0530

    geo loacyionApi integration is successfull

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-// https://geo.ipify.org/
-// const api_key = at_tJWirezro9DWBuDi45jHYzsqA7qTw;;
-//https://geo.ipify.org/api/v2/country,city?apiKey=at_tJWirezro9DWBuDi45jHYzsqA7qTw&ipAddress=8.8.8.8
+const where =async function(ip_ad)
+{
+    const res = await fetch(
+"http://api.ipapi.com/api/"+ip_ad+"?access_key=c4f275366698ec39f39e3dead7936349");
 
-// https://leafletjs.com/
+const data = await res.json();
+console.log(data);
+
+document.querySelector('.info_Ip_val').textContent=data.ip;
+document.querySelector(".info_Country_val").textContent = data.country_name +" ("+ data.location.capital+')';
+document.querySelector(".info_State_val").textContent = data.region_name;
+document.querySelector(".info_City_val").textContent = data.city;
+
+}
+
+const Ipp=document.querySelector('.bar_Search').value;
 
 
-fetch("https://api.ipify.org/").then((r) => r.json()).then(console.log);
+where(Ipp);
